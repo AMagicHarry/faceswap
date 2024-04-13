b4c06d89d42f1bf034e57bac0088ec0364a5c1db
ewan tomson <tomsonewan@gmail.com>
2024-04-12 17:39:38 -0700

    database is added

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 import Replicate from "replicate";
 import request from "request";
 import fs from "fs";
-
+import sqlite3 from "sqlite3";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
+const db = new sqlite3.Database("./database.sqlite");
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN,
 });
 
 async function main() {
-    const mine = "https://i.postimg.cc/bvN4n1hB/male-5.jpg";
+    const query_target = "SELECT * FROM Target ORDER BY RANDOM() LIMIT 1";
 
-    const input = {
-        swap_image: mine, //My Image
-        target_image: "https://i.postimg.cc/WpB9xJN6/handsome-bearded-guy-posing-against-white-wall.jpg"
-    };
+    db.get(query_target, [], (err, row) => {
+        if (err) {
+            throw err;
+        }
 
-    const output = await replicate.run("omniedgeio/face-swap:c2d783366e8d32e6e82c40682fab6b4c23b9c6eff2692c0cf7585fc16c238cfe", { input });
-    console.log(output);
+        const target = row.target_photo;
 
-    request(output).pipe(fs.createWriteStream('image.jpg')).on('close', function () {
-        console.log('Image downloaded!');
+        const mine = "https://i.postimg.cc/bvN4n1hB/male-5.jpg";
+
+        const input = {
+            swap_image: mine,
+            target_image: target,
+        };
+
+        replicate.run("omniedgeio/face-swap:c2d783366e8d32e6e82c40682fab6b4c23b9c6eff2692c0cf7585fc16c238cfe", { input })
+            .then((output) => {
+                request(output).pipe(fs.createWriteStream('image.jpg')).on('close', function () {
+                    console.log('Image downloaded!');
+                });
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+            .finally(() => {
+                db.close();
+            });
     });
 }
 
